f57429eb9f77a9d0f4cbb0fd452268aedd4ca284
sidhantrajput1 <sidhant808310@gmail.com>
2024-10-10 02:05:27 +0530

    display linkedlist

--- LinkedListInJava/Display.java
+++ LinkedListInJava/Display.java
@@ -1,15 +1,15 @@
 package LinkedListInJava;
 
-public class basic {
-    // print linkedlist
+public class Display {
+
     static void display(Node head) {
         Node temp = head;
-        while (temp != null) {
+        while(temp != null) {
             System.out.print(temp.data+" ");
             temp = temp.next;
         }
     }
-
+    
     public static class Node {
         int data; // value
         Node next; // address of next node;
@@ -17,31 +17,19 @@ public class basic {
             this.data = data;
         }
     }
+
     public static void main(String[] args) {
         Node a = new Node(1);
         Node b = new Node(2);
         Node c = new Node(3);
         Node d = new Node(4);
         Node e = new Node(5);
-        // 1 -> 2 -> 3 -> 4
+
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
 
         display(a);
-
-        // Node temp = a;
-        // while (temp != null) {
-        //     System.out.print(temp.data+" ");
-        //     temp = temp.next;
-        // }
-
-        // System.out.println(y.next); // LinkedListInJava.basic$Node@5ea434c8
-        // Node temp = a;
-        // for(int i = 1; i <= 5; i++) {
-        //     System.out.print(temp.data+" ");
-        //     temp = temp.next;
-        // }
     }
 }
