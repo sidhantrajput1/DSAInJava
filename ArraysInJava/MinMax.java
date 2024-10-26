6e6b07193caa013586c5e152f35a4e06fe0d3bac
sidhantrajput1 <sidhant808310@gmail.com>
2024-10-26 22:04:49 +0530

    Min and max element of array

---
+++ ArraysInJava/MinMax.java
@@ -0,0 +1,41 @@
+package ArraysInJava;
+
+public class MinMax {
+    static int max(int[] arr) {
+        int mx = arr[0];
+
+        for(int i = 0; i < arr.length; i++) {
+            if(mx < arr[i]) {
+                mx = arr[i];
+            }
+        }
+
+        return mx;
+    }
+    static int min(int[] arr) {
+        int min = arr[0];
+
+        for(int  i = 0; i< arr.length; i++) {
+            if(min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+    static void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.err.print(arr[i]+ " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 9 ,7 , 8};
+        System.out.print("Array is : ");
+        printArray(arr);
+
+        System.err.println();
+
+        int result = min(arr);
+        System.err.println("Minimum Element of Array is : "+result);
+    }
+}
