1d9814889c7d36d8d80e7fd24e17dfad99f8f5fd
sidhantrajput1 <sidhant808310@gmail.com>
2024-10-26 19:17:04 +0530

    basic stack

---
+++ StackInJava/BasicStack.java
@@ -0,0 +1,19 @@
+package StackInJava;
+import java.util.*;
+
+public class BasicStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        // push for add value in stack
+        st.push(4);
+        st.push(5);
+        st.push(6);
+        st.push(7);
+
+        // Peak for find the value of stack
+        System.err.println(st.peek());
+
+        System.err.println(st);
+    }
+}
