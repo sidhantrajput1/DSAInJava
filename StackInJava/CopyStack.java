0732df2f6152a00fe93548fe6bb40ea4b3d6bf69
sidhantrajput1 <sidhant808310@gmail.com>
2024-11-07 11:29:17 +0530

    Reverse Order

--- StackInJava/CopyStack.java
+++ StackInJava/CopyStack.java
@@ -8,15 +8,36 @@ public class CopyStack {
         Scanner sc= new Scanner(System.in);
         Stack<Integer> st = new Stack<>();
 
-        int n ;
-        System.out.println("Enter the number elements you want to insert : ");
-        n = sc.nextInt();
-        System.out.println("Enter the number element : ");
-
-        for(int i = 1; i <= n; i++) {
-            int x = sc.nextInt();
-            st.push(x);
-        }
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
         System.out.println(st);
+
+        // Reverse Order
+        Stack<Integer> rt = new Stack<>();
+        // while (st.size() > 0) {
+        //     int x = st.peek();
+        //     rt.push(x);
+        //     st.pop();
+        // }
+
+        while (st.size() > 0)  {
+            rt.push(st.pop());
+        }
+
+        System.out.println(rt);
+
+        // int n ;
+        // System.out.println("Enter the number elements you want to insert : ");
+        // n = sc.nextInt();
+        // System.out.println("Enter the number element : ");
+
+        // for(int i = 1; i <= n; i++) {
+        //     int x = sc.nextInt();
+        //     st.push(x);
+        // }
+        // System.out.println(st);
     }
 }
