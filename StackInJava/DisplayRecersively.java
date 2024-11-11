9913901e369a85d9b0312c8c47a20c5b217f0f3e
sidhantrajput1 <sidhant808310@gmail.com>
2024-11-11 20:13:43 +0530

    Display Recursive Stack

---
+++ StackInJava/DisplayRecersively.java
@@ -0,0 +1,25 @@
+package StackInJava;
+
+import java.util.Stack;
+
+public class DisplayRecersively {
+    static void displayReverse(Stack<Integer> st) {
+        if(st.size() == 0) return;
+        int top = st.pop();
+        System.out.print(top+" ");
+        displayReverse(st);
+        st.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+
+        System.out.println(st);
+        displayReverse(st);
+    }
+}
