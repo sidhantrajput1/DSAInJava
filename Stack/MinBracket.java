cfd5814f1b88e1e6583781d56d58a158c0660fb8
sidhantrajput1 <sidhant808310@gmail.com>
2024-11-26 17:55:30 +0530

    find the minimum of bracket that we need to make it given bracket sequence is balanced

---
+++ Stack/MinBracket.java
@@ -0,0 +1,31 @@
+package Stack;
+
+
+// find the minimum of bracket that we need to make it given bracket sequence is balanced
+public class MinBracket {
+    public  static int minBracket(String str) {
+        // Stack<Character> st = new Stack<>();
+        int n = str.length();
+        
+        int balanced = 0;
+        int additions = 0;
+        for(int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                balanced++;
+            } else if (ch == ')') {
+                if (balanced > 0) {
+                    balanced--;
+                } else {
+                    additions++;
+                }
+            }
+        }
+
+        return balanced + additions;
+    }
+    public static void main(String[] args) {
+        String str = "()()))()";
+        System.out.println(minBracket(str));
+    }
+}
