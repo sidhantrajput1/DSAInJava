99b281d976704d7206cc2a429fd49b4078c52f12
sidhantrajput1 <sidhant808310@gmail.com>
2024-12-07 07:23:23 +0530

    ArrayDeque

--- Queue/Basic.java
+++ Queue/Basic.java
@@ -1,22 +1,37 @@
 package Queue;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+// import java.util.LinkedList;
 import java.util.Queue;
 
 public class Basic {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.add(4);
-        q.add(5);
-        System.out.println(q);
-        System.out.println("Size of Queue is : "+q.size());
-        q.remove();
-        System.out.println(q);
-        q.poll();
-        System.out.println(q);
-        System.out.println(q.element());
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> a = new ArrayDeque<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        System.out.println(a);
+        a.poll();
+        System.out.println(a);
+        a.remove();
+        System.out.println(a);
+        System.out.println(a.element());
+        // System.out.println(a.peek());
+
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // System.out.println(q);
+        // System.out.println("Size of Queue is : "+q.size());
+        // q.remove();
+        // System.out.println(q);
+        // q.poll();
+        // System.out.println(q);
+        // System.out.println(q.element());
     }
 }
